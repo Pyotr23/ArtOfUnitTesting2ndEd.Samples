582c0706583552d317155b72966a45a64f382943
Pyotr <2chilavert3@mail.ru>
2021-02-10 20:10:02 +0300

    Тестируемый класс.

---
+++ LogAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArtOfUnitTesting2ndEd.Samples
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (fileName.EndsWith(".SLF"))
+                return false;
+            return true;
+        }
+    }
+}
