4a50c4ccf5584cd208e5b1ac55f5d8da0c8ac640
Pyotr <2chilavert3@mail.ru>
2021-02-12 23:15:39 +0300

    Тестирование состояния калькулятора.

---
+++ LogAn.UnitTests/MemCalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class MemCalculatorTests
+    {
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            var calculator = CreateCalculator();
+
+            var lastSum = calculator.Sum();
+
+            Assert.AreEqual(0, lastSum);
+        }
+
+        [Test]
+        public void Sum_WhenCalled_ChangesSum()
+        {
+
+            var calculator = CreateCalculator();
+
+            calculator.Add(1);
+            var sum = calculator.Sum();
+
+            Assert.AreEqual(1, sum);
+        }
+
+        private MemCalculator CreateCalculator()
+        {
+            return new ();
+        }
+    }
+}
